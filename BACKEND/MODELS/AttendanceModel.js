78253216ef603c239623128f61a083ec583df13e
SABARI-777 <dsabari522@gmail.com>
2026-03-26 22:20:38 +0530

    modify or fixing some errors

---
+++ BACKEND/MODELS/AttendanceModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const AttendanceSchema = new mongoose.Schema(
+  {
+    InDateTime: {
+      type: Date,
+      required: false,
+      default: Date.now,
+    },
+    StudentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    Status: {
+      type: String,
+      default: "OUT",
+    },
+    RoomId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+    EntryType: {
+      type: String,
+      enum: ["BIOMETRIC", "MANUAL"],
+      default: "BIOMETRIC",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AttendanceDetails = mongoose.models.AttendanceDetails || mongoose.model("AttendanceDetails", AttendanceSchema, "AttendanceDetails");
+
+export default AttendanceDetails;
---
+++ BACKEND/MODELS/PlacementAttendanceModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const PlacementAttendancePassSchema = new mongoose.Schema(
+  {
+    OutDateTime: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    InDateTime: {
+      type: Date,
+      required: false,
+      default: null,
+    },
+    Status: {
+      type: String,
+      enum: ["OUT","IN"],
+      default: "OUT",
+    },
+    StudentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    RoomId:{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Room",
+          required: true,
+        },
+    PlacementId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Placement",
+      require:true,
+    },
+     EntryType: {
+      type: String,
+      enum: ["BIOMETRIC", "MANUAL"],
+      default: "BIOMETRIC",
+    },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PlacementAttendanceDetails = mongoose.models.PlacementAttendanceDetails || mongoose.model("PlacementAttendanceDetails", PlacementAttendancePassSchema, "PlacementAttendanceDetails");
+
+export default PlacementAttendanceDetails;
